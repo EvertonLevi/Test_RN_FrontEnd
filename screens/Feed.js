119fef059a285888087a4f0491bc2ba4c0596782
Everton__Levi <everton.levi@hotmail.com>
2020-05-05 00:53:43 -0300

    Init Feed page

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,26 +1,60 @@
 import * as React from 'react';
 import {
- Image, Platform, StyleSheet, Text,
- TouchableOpacity, View
+ Image, Platform, StyleSheet, Text, Alert,
+ TouchableOpacity, View, Dimensions
 } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons'
 import Constants from 'expo-constants'
 import colors from '../constants/Colors'
 
+const { width } = Dimensions.get("window")
+
 export default function Feed() {
  return (
   <View style={styles.container}>
-   <View style={styles.header}>
-    <Text style={styles.textHeader}>RIDERIZE</Text>
+   <View style={styles.headerFull}>
 
-    <TouchableOpacity >
-     <Feather name="magnifying-glass" size={28} color="#8f5de8" />
-    </TouchableOpacity>
+    <View style={styles.header}>
+     <Text style={styles.textHeader}>RIDERIZE</Text>
 
-    <TouchableOpacity >
-     <Feather name="send-o" size={28} color="#8f5de8" />
-    </TouchableOpacity>
+     <TouchableOpacity >
+      <Feather name="magnifying-glass" size={28} color="#8f5de8" />
+     </TouchableOpacity>
+
+     <TouchableOpacity >
+      <Feather name="send-o" size={28} color="#8f5de8" />
+     </TouchableOpacity>
+    </View>
+
+    <View style={styles.buttonGroup}>
+     <TouchableOpacity
+      style={styles.segmentButton}
+      onPress={() => Alert.alert("Seguindo")}
+     >
+      <Text style={styles.textSegmentButton}>Seguindo</Text>
+     </TouchableOpacity>
+     <TouchableOpacity
+      style={styles.segmentButton}
+      onPress={() => Alert.alert("Clubes")}
+     >
+      <Text style={styles.textSegmentButton}>Clubes</Text>
+     </TouchableOpacity>
+     <TouchableOpacity
+      style={styles.segmentButton}
+      onPress={() => Alert.alert("Marcas")}
+     >
+      <Text style={styles.textSegmentButton}>Marcas</Text>
+     </TouchableOpacity>
+    </View>
+
+    <View>
+     <TouchableOpacity style={styles.buttonNewPost}>
+      <Text style={styles.textButtonNewPost}>
+       Criar nova publicação <Feather name="plus" size={15} color="#fff" />
+      </Text>
+     </TouchableOpacity>
+    </View>
    </View>
 
   </View>
@@ -32,13 +66,22 @@ const styles = StyleSheet.create({
  container: {
   paddingTop: Constants.statusBarHeight + 20,
   flex: 1,
-  backgroundColor: '#fff',
   alignItems: "center",
+  backgroundColor: colors.backCinzaEscuro
+ },
+ headerFull: {
+  backgroundColor: '#fff',
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: 'center',
+  width: width,
+  height: 230,
  },
  header: {
   flexDirection: "row",
   justifyContent: "space-between",
-  alignItems: 'center'
+  alignItems: 'center',
+  marginBottom: 10,
  },
  textHeader: {
   fontSize: 30,
@@ -49,4 +92,35 @@ const styles = StyleSheet.create({
  headerTextBold: {
   fontWeight: "bold"
  },
+ buttonGroup: {
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between"
+ },
+ segmentButton: {
+  paddingVertical: 15,
+  paddingHorizontal: 30,
+ },
+ textSegmentButton: {
+  fontSize: 14,
+  color: colors.roxo,
+  fontWeight: "bold",
+  borderBottomWidth: 2,
+  borderBottomColor: colors.roxo
+ },
+ buttonNewPost: {
+  marginVertical: 18,
+  backgroundColor: colors.roxo,
+  height: 40,
+  width: width - 130,
+  borderRadius: 12,
+  justifyContent: "center",
+  alignItems: "center"
+ },
+ textButtonNewPost: {
+  color: "#fff",
+  fontSize: 13,
+  fontWeight: "bold",
+ },
+
 });
